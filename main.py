2351ae947847c1eb68875ada2aa2390435f127b0
akankshanehete <akankshanehete8@gmail.com>
2021-12-28 11:33:52 -0500

    finished turtle race

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Turtle, Screen
+import random
+
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
-screen.exitonclick()
 user_bet = screen.textinput(title="Make your bet!",prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
+print("Your bet is: " +user_bet)
 red = Turtle(shape="turtle")
 orange = Turtle(shape="turtle")
 yellow = Turtle(shape="turtle")
@@ -17,6 +19,7 @@ def initialise_turtle(turtle,y_coord,color):
     turtle.penup()
     turtle.color(color)
     turtle.goto(-230,y_coord)
+    turtle.speed("slow")
 
 
 initialise_turtle(red,-150,'red')
@@ -26,6 +29,27 @@ initialise_turtle(green,0,'green')
 initialise_turtle(blue,50,'blue')
 initialise_turtle(indigo,100,'indigo')
 initialise_turtle(violet,150,'violet')
+
+all_turtles = [red, orange, yellow, green, blue, indigo, violet]
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on == True:
+    for turtle in all_turtles:
+        turtle.forward(random.randint(0,10))
+        if turtle.xcor() > 230:
+            is_race_on = False
+            print(turtle.color()[1] + " won the race!")
+            if turtle.color()[1] == user_bet:
+                print("You won the bet!")
+            else:
+                print("You lost the bet :(")
+
+
+
+
+screen.exitonclick()
 
 
 
